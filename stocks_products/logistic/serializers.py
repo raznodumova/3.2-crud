baa11b4ac8d9901b53399e7b95af70d752dfb240
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-11-25 17:57:11 +0300

    исправила недочеты

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -37,12 +37,10 @@ class StockSerializer(serializers.ModelSerializer):
         # с помощью списка positions
 
         for position in positions:
-            StockProduct.objects.create(
-                stock=stock,
-                product=position['product'],
-                quantity=position['quantity'],
-                price=position['price']
-            )
+            StockProduct.objects.update_or_create(stock=stock,
+                                                  product=position['product'],
+                                                  defaults ={"quantity": position['quantity'],
+                                                             "price": position['price']})
 
         return stock
 
