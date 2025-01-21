849bcc9cc2b0bd80e82c7eb2bb8bb62cc0d91d83
Полина Тригуб PD-87 <doc21@inbox.ru>
2025-01-21 16:32:59 +0300

    Docker

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'netology_stocks_products',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': '5728821q',
     }
 }
 
