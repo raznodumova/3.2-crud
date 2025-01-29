dd805c6325a96d021d30a58763a629b34ded7610
Полина Тригуб PD-87 <doc21@inbox.ru>
2025-01-29 22:22:54 +0300

    Docker-compose

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
+        'HOST': 'db',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '5728821q',
     }
 }
 
@@ -123,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
